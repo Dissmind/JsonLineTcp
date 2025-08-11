73bedb068e22491269e8635a6057d94382353139
Alexey <DissMind@gmail.com>
2025-08-11 03:20:29 +0300

    [ADD](Client): init IMessageSender

---
+++ JsonLineTcp.Client/IMessageSender.cs
@@ -0,0 +1,8 @@
+﻿using JsonLineTcp.Client.Common;
+
+namespace JsonLineTcp.Client.Intrefaces;
+
+public interface IMessageSender
+{
+    Task<int> SendAllAsync(ILineSource lines, IJsonValidator validator, IConnection connection, CancellationToken cancellationToken);
+}
